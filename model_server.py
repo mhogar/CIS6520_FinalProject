49dd9eaaa6e8a87b7b2e993e4a35023a7bf05b4d
Ryan <mhogar98@gmail.com>
2022-03-09 11:49:45 -0800

    Implement NN based classifier + separate sever and model prediction code

--- model_server.py
+++ model_server.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import json
+import model
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from joblib import load
+
 
 class Handler(BaseHTTPRequestHandler):
 	def __init__(self, *args):
-		self.clf = load("model/model.dat")
-		with open("model/labels.csv") as file:
-			self.labels = file.readline().split(",")
-
+		self.model = model.Model()
 		BaseHTTPRequestHandler.__init__(self, *args)
 
 
@@ -24,22 +21,11 @@ class Handler(BaseHTTPRequestHandler):
 		length = int(self.headers['Content-Length'])
 		post_data = self.rfile.read(length)
 
-		response = {
-			'type': self.determine_block_type(post_data)
-		}
+		response = self.model.predict(post_data)
 
 		self._set_response()
 		self.wfile.write(f"{json.dumps(response)}\n".encode('utf-8'))
-
-
-	def determine_block_type(self, data):
-		vec = np.zeros(256, dtype=np.int16)
-		for byte in data:
-			vec[byte] += 1
-
-		prediction = self.clf.predict([vec])[0]
-		return self.labels[prediction]
-
+	
 
 def run(port=8080):
 	server_address = ('192.168.1.12', port)
@@ -55,7 +41,7 @@ def run(port=8080):
 if __name__ == '__main__':
 	from sys import argv
 
-	if len(argv) == 2:
+	if len(argv) >= 2:
 		run(port=int(argv[1]))
 	else:
 		run()
